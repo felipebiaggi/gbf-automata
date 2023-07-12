a11ccb09fd863ed7f87cfa5eecb2a6e985816013
Felipe Biaggi Carvalho <felipebiaggi@gmail.com>
2023-07-12 10:24:33 -0300

    first commit

---
+++ gbf_ocr/main.py
@@ -0,0 +1,31 @@
+import numpy as np
+import cv2 as cv
+import pyautogui
+from PIL import Image
+from PIL import ImageGrab
+from pyscreeze import screenshot
+
+def start():
+    # image = pyautogui.screenshot()
+
+    # image = cv.cvtColor(np.array(image),
+                     # cv.COLOR_RGB2BGR)
+   
+    screenshot = ImageGrab.grab(
+        bbox=None,
+        include_layered_windows=False,
+        all_screens=True
+    )
+    
+    screenshot.show()
+    
+    # im = Image.open(image)
+
+    # im.show()
+    
+
+    # cv.imshow('Teste SS', image)
+    #
+     # cv.waitKey(0)
+    # 
+    # cv.destroyAllWindows()
---
+++ tests/template_matching.py
@@ -0,0 +1,53 @@
+import argparse
+import cv2 as cv
+from pathvalidate.argparse import sanitize_filepath_arg
+from matplotlib import pyplot as plt
+
+parse = argparse.ArgumentParser(
+    prog="Template matching",
+    description="Find objects in an image using template matching method"
+)
+parse.add_argument(
+    "-i", "--image", type=sanitize_filepath_arg
+)
+parse.add_argument(
+    "-t", "--template", type=sanitize_filepath_arg
+)
+
+if __name__ == "__main__":
+    args = parse.parse_args()
+    
+    image_rgb = cv.imread(
+        args.image
+    )
+
+    image_gray = cv.cvtColor(image_rgb, cv.COLOR_BGR2GRAY)
+
+    template = cv.imread(
+        args.template,
+        cv.IMREAD_UNCHANGED
+    )
+
+    w, h = template.shape[::-1]
+
+    method = cv.TM_SQDIFF
+    
+    res = cv.matchTemplate(
+        image_gray,
+        template,
+        method
+    )
+
+    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
+
+    top_left = min_loc
+    bottom_right = (top_left[0] + w, top_left[1] + h)
+
+    cv.rectangle(image_rgb, top_left, bottom_right, (0, 0, 255), 4)
+
+    plt.imshow(cv.cvtColor(image_rgb, cv.COLOR_BGR2RGB))
+    plt.xticks([])
+    plt.yticks([])
+
+    plt.show()
+
