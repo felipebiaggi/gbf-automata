660c4aa0ce3a17ec6db8fc7ae5004e3c293fb938
Felipe Biaggi Carvalho <felipebiaggi@gmail.com>
2023-10-20 18:09:20 -0300

    migrate extension

---
+++ extension/content/content.js
@@ -0,0 +1,20 @@
+function sendMsg(msg){
+  chrome.runtime.sendMessage({
+    message: msg
+  })
+}
+
+const target = document.querySelector('.img-load');
+
+const observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      if (target.style.cssText === 'display: none;'){ 
+        sendMsg("Loading Finished.")
+      }
+    });
+
+});
+
+const config = { attributes: true, attributeOldValue: true };
+
+observer.observe(target, config);
