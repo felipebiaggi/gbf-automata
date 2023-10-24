44408db0f4cb92ad43ed752e3f7c9d0a29cd3f35
Felipe Biaggi Carvalho <felipebiaggi@gmail.com>
2023-10-23 23:11:01 -0300

    add websocket server

--- extension/background/worker.js
+++ extension/background/worker.js
@@ -1,3 +1,18 @@
+import './lib/socket.io.js';
+
+const socket = io('127.0.0.1:65432', {
+  jsonp: false,
+});
+
+socket.on('connect', ()=>{
+  console.log("Connection successful")
+});
+
+socket.on("connect_error", (error) => {
+  console.log(`Connection error: <${error}>`)
+});
+
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    console.log(message);
+    console.log(socket);
+    socket.emit("Teste")
   });
--- src/gbf_automata/main.py
+++ src/gbf_automata/main.py
@@ -1,3 +1,6 @@
+import asyncio
+import time
+from websockets.server import serve
 import multiprocessing
 import socket
 import selectors
@@ -8,88 +11,129 @@ HOST = "127.0.0.1"
 PORT = 65432
 
 sel = selectors.DefaultSelector()
+#
+#
+# def start_bot():
+#     
+#
 
 
-def start_bot():
-    print(f"foo: <{multiprocessing.current_process()}>")
+# def accept_wrapper(sock):
+#     conn, addr = sock.accept()  # Should be ready to read
+#     print(f"Accepted connection from {addr}")
+#     conn.setblocking(False)
+#     data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
+#     events = selectors.EVENT_READ | selectors.EVENT_WRITE
+#     sel.register(conn, events, data=data)
+#
+#
+# def service_connection(key, mask):
+#     sock = key.fileobj
+#     data = key.data
+#     if mask & selectors.EVENT_READ:
+#         recv_data = sock.recv(1024)  # Should be ready to read
+#         if recv_data:
+#             data.outb += recv_data
+#         else:
+#             print(f"Closing connection to {data.addr}")
+#             sel.unregister(sock)
+#             sock.close()
+#     if mask & selectors.EVENT_WRITE:
+#         if data.outb:
+#             print(f"Teste")
+#             sent = sock.send()  # Should be ready to write
+#             data.outb = data.outb[sent:]
+#
+#
+# def create_tcp_server():
+#     slock = socket.socket(socket.AF_, socket.SOCK_STREAM)
+#     slock.bind((HOST, PORT))
+#     slock.listen()
+#     pprint.pprint(f"Listening on {(HOST, PORT)}")
+#     slock.setblocking(False)
+#     sel.register(slock, selectors.EVENT_READ, data=None)
+#
+#     try:
+#         while True:
+#             events = sel.select(timeout=None)
+#             for key, mask in events:
+#                 if key.data is None:
+#                     accept_wrapper(key.fileobj)
+#                 else:
+#                     service_connection(key, mask)
+#
+#     except KeyboardInterrupt:
+#         pprint.pprint("Caught keyboard interrupt, exiting")
+#     finally:
+#         sel.close()
 
 
-def accept_wrapper(sock):
-    conn, addr = sock.accept()  # Should be ready to read
-    print(f"Accepted connection from {addr}")
-    conn.setblocking(False)
-    data = types.SimpleNamespace(addr=addr, inb=b"", outb=b"")
-    events = selectors.EVENT_READ | selectors.EVENT_WRITE
-    sel.register(conn, events, data=data)
+# def create_udp_server():
+#     socket_lock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#     socket_lock.bind((HOST, PORT))
+#     pprint.pprint(f"Listening on {(HOST, PORT)}")
+#
+#     try:
+#         while(True):
+#             payload, _ = socket_lock.recvfrom(1)
+#
+#             print(payload)
+#
+#
+#     except KeyboardInterrupt:
+#         pprint.pprint("Caught keyboard interrupt, exiting")
+#     finally:
+#         socket_lock.close()
 
 
-def service_connection(key, mask):
-    sock = key.fileobj
-    data = key.data
-    if mask & selectors.EVENT_READ:
-        recv_data = sock.recv(1024)  # Should be ready to read
-        if recv_data:
-            data.outb += recv_data
-        else:
-            print(f"Closing connection to {data.addr}")
-            sel.unregister(sock)
-            sock.close()
-    if mask & selectors.EVENT_WRITE:
-        if data.outb:
-            print(f"Echoing {data.outb!r} to {data.addr}")
-            sent = sock.send(data.outb)  # Should be ready to write
-            data.outb = data.outb[sent:]
 
 
-def create_socket():
-    slock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    slock.bind((HOST, PORT))
-    slock.listen()
-    pprint.pprint(f"Listening on {(HOST, PORT)}")
+async def echo(websocket):
+    async for message in websocket:
+        pprint.pprint(f"Message: {message} - {time.time()}")
+        await websocket.send(message)
 
-    slock.setblocking(False)
-    sel.register(slock, selectors.EVENT_READ, data=None)
-
-    try:
-        while True:
-            events = sel.select(timeout=None)
-            for key, mask in events:
-                if key.data is None:
-                    accept_wrapper(key.fileobj)
-                else:
-                    service_connection(key, mask)
-
-    except KeyboardInterrupt:
-        pprint.pprint("Caught keyboard interrupt, exiting")
-
-    finally:
-        sel.close()
+async def websocket_server():
+    pprint.pprint(f"Create Websocket Server: {HOST}:{PORT}")
+    async with serve(echo, HOST, PORT):
+        await asyncio.Future()
 
 
 class LoadState(object):
     def __init__(self) -> None:
         self._state = None
+        self._lock = multiprocessing.Lock()
 
     @property
     def state(self):
-        return self._state
+        with self._lock:
+            return self._state
 
     @state.setter
     def state(self, value):
-        print("State Change!")
-        self._state = value
+        with self._lock:
+            print("State Change!")
+            self._state = value
 
+    @state.getter
+    def state(self):
+        with self._lock:
+            return self._state
+
+
+def run_websocket_server():
+    pprint.pprint(f"Server Process INFO: {multiprocessing.current_process()}")
+    asyncio.run(websocket_server())
 
 if __name__ == "__main__":
+    
+    server_process = multiprocessing.Process(target=run_websocket_server)
+    try:
+        server_process.start()
+    except KeyboardInterrupt:
+        pprint.pprint("Caught keyboard interrupt, exiting")
 
-    load_state: LoadState = LoadState()
-
-    load_state = "finished"
-
-    # create_socket()
-
-    # game = GBFGame()
-    # game.start()
+    server_process.join()
 
     # print(f"main: <{threading.get_ident()}>")
     #
@@ -97,4 +141,3 @@ if __name__ == "__main__":
     #
     # bot_process.start()
     # bot_process.is_alive()
-    pass
